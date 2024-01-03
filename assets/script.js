060f577c7a8b97e0c2b46519e15a1111fc768a87
Jerôme Duprez <jeromeduprez@outlook.fr>
2024-01-03 01:13:14 +0100

    Arrow left

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,39 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowRight = document.getElementById("arrow_Right");
+const arrowLeft = document.getElementById("arrow_Left");
+const imgSelected = document.getElementById("img-selected");
+const textSelected = document.getElementById("text");
+let index = 0;
+const Dot0 = document.getElementById("dot0");
+const Dot1 = document.getElementById("dot1");
+const Dot2 = document.getElementById("dot2");
+const Dot3 = document.getElementById("dot3");
+
+
+arrowRight.onclick = function() {
+	index +=1;
+	if (index === 4) {index = 0};
+	imgSelected.src = "assets/images/slideshow/" + slides[index].image;
+	textSelected.innerHTML = slides[index].tagLine;
+	for (i=0;i<slides.length;i++) {
+		eval('Dot'+i+'.className = "dot"')
+	};
+	eval('Dot'+index+'.className = "dot dot_selected"');
+	console.log(index);
+};
+
+arrowLeft.onclick = function() {
+	index -=1;
+	if (index === -1) {index = 3};
+	imgSelected.src = "assets/images/slideshow/" + slides[index].image;
+	textSelected.innerHTML = slides[index].tagLine;
+	for (i=0;i<slides.length;i++) {
+		eval('Dot'+i+'.className = "dot"')
+	};
+	eval('Dot'+index+'.className = "dot dot_selected"');
+	console.log(index);
+};
+
